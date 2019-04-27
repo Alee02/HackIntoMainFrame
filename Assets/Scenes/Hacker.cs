4df529a149139a494ec860edef4519cc76be4f5b
Alee02 <a-shah@hotmail.co.uk>
2019-04-27 12:57:34 +0100

    Initital Setup

---
+++ Assets/Scenes/Hacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("Hello Hacker");
+        Terminal.WriteLine("Welcome to Hack into mainframe");
+        Terminal.WriteLine("Are you ready?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
