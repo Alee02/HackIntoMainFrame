fdb66d60bb25e864a10784499e2fcaf3bd77ff43
Alee02 <a-shah@hotmail.co.uk>
2019-04-28 22:41:09 +0100

    Code clean up

--- Assets/Scenes/Hacker.cs
+++ Assets/Scenes/Hacker.cs
@@ -12,6 +12,9 @@ public class Hacker : MonoBehaviour
     int category;
     int gameTimer;
     Boolean isWinner = false;
+    int gameTries = 5;
+    string secretWord = "";
+    string[] nasa = { "Absolute Humidity", "Absorption","Acid Rain","Afforestation","Albedo","Algorithm" };
 
     enum State
     {
@@ -30,26 +33,6 @@ public class Hacker : MonoBehaviour
         loginScreen();
     }
 
-    void startGame()
-    {
-        Terminal.WriteLine("Game started");
-        if (level == 1)
-        {
-            startLevel1();
-        }
-        else
-        {
-            Terminal.WriteLine("You didn't pick level 1");
-        }
-
-    }
-
-    void startLevel1()
-    {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Level 1 Started");
-    }
-
     void loginScreen()
     {
         Terminal.ClearScreen();
@@ -64,7 +47,6 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("Hello " + greeting);
         Terminal.WriteLine("Welcome to Hack into mainframe");
         Terminal.WriteLine("What would you like to hack into?");
-        Terminal.WriteLine("Press 1 for Variables");
         Terminal.WriteLine("Press 2 for Main Frame");
     }
 
@@ -101,65 +83,75 @@ public class Hacker : MonoBehaviour
     {
         gameState = State.Category;
         Terminal.ClearScreen();
-        Terminal.WriteLine("Choose your target...");
-        Terminal.WriteLine("Press 1 for the local library");
-        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Choose your target (difficulty)...");
+        Terminal.WriteLine("Press 1 for nasa");
     }
 
     void OnUserInput(string input)
     {
-        print(gameState);
-        if (gameState == State.Login)
+        if(gameTries > 0)
         {
-            // Login Screen
-            if (input.Length > 0)
+            if (gameState == State.Login)
             {
-                print("User input");
-                print("We in login state and setting stuff");
-                username = input;
-                showMainMenu(username);
-            }
-        }
-        else if (gameState == State.Menu)
-        {
-            if (input.Length > 0)
-            {
-                var x = 0;
-                if (int.TryParse(input, out x))
+                // Login Screen
+                if (input.Length > 0)
                 {
-                    level = x;
-                    ShowCategoryScreen();
+                    print("User input");
+                    print("We in login state and setting stuff");
+                    username = input;
+                    showMainMenu(username);
                 }
             }
-        } else if (gameState == State.Category)
-        {
-            if (input.Length > 0)
+            else if (gameState == State.Menu)
             {
-                string secretWord = CreateAnagram("mainframe");
-                StartGame(secretWord);
+                if (input.Length > 0)
+                {
+                    var x = 0;
+                    if (int.TryParse(input, out x))
+                    {
+                        level = x;
+                        ShowCategoryScreen();
+                    }
+                }
             }
-        }
-        else if (gameState == State.Game){
-           if(input == "mainframe")
+            else if (gameState == State.Category)
             {
-                Terminal.WriteLine("You hacked into the mainframe");
-                isWinner = true;
-                gameState = State.Won;
-            } else
-            {
-                Terminal.WriteLine("Oh no, the cops caught you!");
-                gameState = State.Lost;
-                
+                if (input.Length > 0)
+                {
+                    secretWord = CreateAnagram(nasa[UnityEngine.Random.Range(0, nasa.Length)]);
+                    string codedSecretWord = CreateAnagram(secretWord);
+                    StartGame(codedSecretWord);
+                }
             }
-        }
-        else if (gameState == State.Won)
+            else if (gameState == State.Game)
+            {
+                if (nasa (secretWord))
+                {
+                    Terminal.WriteLine("You hacked into the mainframe");
+                    Terminal.WriteLine("Welcome to Nasa");
+                    Terminal.WriteLine(@"
+ _ __   __ _ ___  __ _ 
+| '_ \ / _` / __|/ _` |
+| | | | (_| \__ \ (_| |
+|_| |_|\__,_|___/\__,_|
+                       
+                       
+");
+                    isWinner = true;
+                }
+                else
+                {
+                    Terminal.WriteLine("Not quite it, Try Again");
+                    gameTries--;
+                }
+            } else if (gameState == State.Game && isWinner)
+            {
+                Terminal.WriteLine("You've have retrieved a ");
+            }
+        } else
         {
-            gameState = State.Menu;
-        } else if (gameState == State.Lost)
-        {
-            string secretWord = CreateAnagram("Astrnaut");
-            Terminal.WriteLine("You can't type from a jail cell");
-            StartGame(secretWord);
+            Terminal.WriteLine("Oh no, the cops caught you!");
+            Terminal.WriteLine("Type breakout to start again.");
         }
     }
 }
